aafc0fbf5d70e6906b0c18b3da7076b1a86b451f
RohanKatreddy <rohan.katreddy@gmail.com>
2024-07-24 13:58:05 -0300

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -23,7 +30,7 @@ const useGames = () => {
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
-        if (err instanceof CancelError) return;
+        if (err instanceof CanceledError) return;
         setError(err.message);
       });
 
